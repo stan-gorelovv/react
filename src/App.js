e361854b6be0e272982b3ce2a70b578d1ddfd5a7
stan-gorelovv <stan.gorelovv@yandex.ru>
2021-04-05 12:22:17 +0300

    Commit react_v0.1 Вывод одного поста, удаление поста

--- src/App.js
+++ src/App.js
@@ -1,64 +1,22 @@
-//import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import {Route, BrowserRouter, NavLink} from "react-router-dom";
-
-function Menu(){
-        return(
-            <nav className="nav">
-                <NavLink className="nav-link active" aria-current="page" to="/">Main</NavLink>
-                <NavLink className="nav-link" to="/about">About us</NavLink>
-                <NavLink className="nav-link" to="/contact-us">Contact us</NavLink>
-            </nav>
-        )
-}
-
-function ContactUs(){
-    return (
-        <div className="container my-5">
-                <form action="">
-                    <div className="mb-3"><input type ="text" className="form-control"/></div>
-                    <div className="mb-3"><input type ="text" className="form-control"/></div>
-                    <div className="mb-3"><textarea type ="text" className="form-control"/></div>
-                    <div className="mb-3"><input type ="text" className="form-control btn btn-primary"/></div>
-
-                </form>
-        </div>
-    )
-}
-
-
-function Main(){
-    return (
-        <div className="container my-5">
-            <form action="">
-                <h1>Main Page</h1>
-            </form>
-        </div>
-    )
-}
-
-function AboutUs(){
-    return (
-        <div className="container my-5">
-            <form action="">
-                <h1>About us</h1>
-            </form>
-        </div>
-    )
-}
+import {Menu} from "./components/Menu";
+import {PostList} from "./components/PostList";
+import {Post} from "./components/Post";
+import {AddPost} from "./components/AddPost";
 
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-            <Menu/>
-
-            <Route exact path="/" render={()=><Main/>} />
-            <Route path="/about" render={()=><AboutUs/>}/>
-            <Route path="/contact-us" render={()=><ContactUs/>}/>
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="container">
+            <BrowserRouter>
+                <Menu/>
+                <Route exact path="/" render={()=><PostList/>} />
+                <Route path="/addPost" render={()=><AddPost/>}/>
+                <Route path="/post/:id" render={(props)=><Post {...props}/>}/>
+            </BrowserRouter>
+        </div>
+    );
 }
+
 export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+function Tr(props){
+    return <tr>
+        <th scope="row">{props.index} [{props.id}]</th>
+        <td><Link to={"/post/"+props.id}>{props.title}</Link></td>
+        <td>{props.author}</td>
+        <td>{props.date_added}</td>
+        <td><span className='delete-post-btn' onClick={()=>{
+            const formData = new FormData();
+            formData.append('id',props.id);
+            fetch('http://201.vozhzhaev.ru/php/removePost.php',{
+                method: "POST",
+                body: formData
+            }).then(response=>response.json())
+                .then(result=>{
+                    let posts = props.parent.state.posts;
+                    posts.splice(props.index-1, 1);
+                    props.parent.setState({
+                        posts:posts
+                    })
+                })
+        }
+        }>[Удалить]</span></td>
+    </tr>
+}
+
+export class PostList extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            posts: []
+        }
+    }
+    componentDidMount() {
+        console.log("Компонет PostList отрисован");
+        fetch("http://201.vozhzhaev.ru/php/getPosts.php")
+            .then(response=>response.json())
+            .then(result=>{
+                console.log(result);
+                let rows = [];
+                for (let i = 0; i < result.length; i++) {
+                    rows.push(<Tr
+                        key={i}
+                        index={i+1}
+                        id={result[i].id}
+                        title={result[i].title}
+                        author={result[i].author}
+                        date_added={result[i].date_added}
+                        parent={this}
+                    />)
+                }
+                this.setState({
+                    posts: rows
+                })
+            })
+    }
+
+    render() {
+        console.log("Компонет PostList рисуется");
+        return <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Заголовок</th>
+                <th scope="col">Автор</th>
+                <th scope="col">Дата добавления</th>
+                <th scope="col">Управление</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.state.posts}
+            </tbody>
+        </table>
+    }
+}
